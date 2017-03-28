5daee3ced359970b905af1ba555ba62502f64c01
regnat <rg@regnat.ovh>
2017-03-28 15:14:06 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{
+  nixpkgs ? <nixpkgs>, system ? builtins.currentSystem
+}:
+with import nixpkgs { inherit system; };
+stdenv.mkDerivation rec {
+  name = "papers";
+  version = "0.0";
+  buildInputs = [
+      (texlive.combine {
+        inherit (texlive) scheme-medium syntax;
+      })
+      biber fontconfig pdfpc rubber
+  ];
+
+  src = ./.;
+}
+
