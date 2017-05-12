1114b37de650874885473ef1a26b52dfd7ff2cf6
regnat <rg@regnat.ovh>
2017-05-12 16:29:37 +0200

    Add installphase for hydra builds
    Also takes care of declaring hydra products for download

--- default.nix
+++ default.nix
@@ -12,5 +12,11 @@ stdenv.mkDerivation rec {
   ];
 
   src = ./.;
+
+  installPhase = ''
+    mkdir -p $out/nix-support
+    cp out/main.pdf $out/
+    echo "file pdf $out/main.pdf" >> $out/nix-support/hydra-build-products
+  '';
 }
 
