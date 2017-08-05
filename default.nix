462e5050423b5a78aa1edcb21af9c36de2ce449b
regnat <rg@regnat.ovh>
2017-08-05 12:02:18 +0200

    Add the needed fonts in the Nix environment
    Hydra builds fails without this although it locally builds in an impure
    shell

--- default.nix
+++ default.nix
@@ -2,17 +2,22 @@
   nixpkgs ? <nixpkgs>, system ? builtins.currentSystem
 }:
 with import nixpkgs { inherit system; };
+let myTexLive =
+  texlive.combine {
+    inherit (texlive) scheme-medium syntax todo appendix paralist csvsimple
+    biblatex logreq xstring cleveref filehook;
+  };
+in
 stdenv.mkDerivation rec {
   name = "rapport-stage";
   version = "2017";
   buildInputs = [
-      (texlive.combine {
-        inherit (texlive) scheme-medium syntax todo appendix paralist csvsimple
-        biblatex logreq xstring cleveref filehook;
-      })
+      myTexLive
       pandoc
       biber
   ];
+
+  FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ "${myTexLive}/share/texmf/" ]; };
 
   src = builtins.filterSource (name: type:
     let baseName = baseNameOf (toString name); in !(
